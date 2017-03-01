022084112ae028f49863b80512646296be4f500b
Haitham Ennasr <haitham@llcl.lc>
2017-02-28 21:17:26 -0500

    Styled language selector for mobile vew, added golbal styles

--- web/app/themes/palfest/templates/header.php
+++ web/app/themes/palfest/templates/header.php
@@ -6,7 +6,7 @@
 <header class="navbar navbar-toggleable-md navbar-light bg-faded" role="banner">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target=".navbar-collapse">
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
