f88bf62fec6b8bd420cd4899e87048767c4dfd3f
Haitham Ennasr <haitham@llcl.lc>
2017-03-01 08:59:41 -0500

    tweaked mobile and tablet menu, centered logo on desktop menu

--- web/app/themes/palfest/templates/header.php
+++ web/app/themes/palfest/templates/header.php
@@ -7,13 +7,13 @@
   <div class="container-fluid">
     <div class="navbar-header">
     <div class = "row">
-    <div class ="col-6 hidden-lg hamburger">
+    <div class ="col-6 col-lg-4 hidden-lg hamburger">
       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
 
       </button>
       </div>
-      <div class="navbar-brand col-4">
+      <div class="navbar-brand col-5 col-lg-4">
       <a  href="<?= esc_url(home_url('/')); ?>"><img src="<?= get_template_directory_uri() . '/dist/images/
 logo.jpg'; ?>"></a>
 </div>
