24fb1507f1efcaf9b32f71bcb667b584fd56f640
Haitham Ennasr <haitham@llcl.lc>
2017-03-06 10:10:02 -0500

    fornt page splash screen styling done, need to check on xl screens

--- web/app/themes/palfest/front-page.php
+++ web/app/themes/palfest/front-page.php
@@ -1,6 +1,8 @@
 
 <div id= "front-page">
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+
+	  		<?php get_template_part('templates/content', 'page'); ?>
+
 <?php endwhile; ?>
 </div>
