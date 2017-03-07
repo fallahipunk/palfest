941f4319ef65c54eeac809e10631abeaf5ec1ec2
Haitham Ennasr <haitham@llcl.lc>
2017-03-07 17:45:47 -0500

    started working on homepage mailing list and mission statement

--- web/app/themes/palfest/front-page.php
+++ web/app/themes/palfest/front-page.php
@@ -6,3 +6,14 @@
 
 <?php endwhile; ?>
 </div>
+	  		<div class = "row">
+	  		
+		  		<div class = "col-md-6 homepage-newsletter">
+		  		hi hi hi
+		  		</div>
+
+				<div class = "col-md-6 homepage-mission">
+				hello hello
+	  			</div>
+
+	  		</div>
