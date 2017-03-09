3af1d0df335fa590adb6cc9794d13c2c1cb4e5a8
Haitham Ennasr <haitham@llcl.lc>
2017-03-09 17:02:24 -0500

    added about excerpt funtionality to homepage

--- web/app/themes/palfest/templates/about_excerpt-ar.php
+++ web/app/themes/palfest/templates/about_excerpt-ar.php
@@ -1,1 +1,18 @@
-مرحبا يا اهلا وسهلا
+<?php 
+
+$page = get_page_by_title( 'من نحن' );
+$the_excerpt = $page->post_excerpt;
+$page_data = get_page( $page );
+$title = $page_data->post_title;
+
+?>
+
+    <div class="entry-content">
+        <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'About' ) ) );?>"> 
+        <?php 
+        	echo $the_excerpt; 
+        ?>
+        <br>
+        +المزيد
+        </a>
+    </div>
