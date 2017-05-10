0cef963c8613b090efb1bbda57a78c76bce167ad
Haitham Ennasr <haitham@llcl.lc>
2017-05-10 13:20:37 -0400

    added feature image bg functionality to opening statement and programme

--- web/app/themes/palfest/page.php
+++ web/app/themes/palfest/page.php
@@ -1,5 +1,27 @@
-<div class = "palfest-content">
+
 <?php while (have_posts()) : the_post(); ?>
+
+<?php              
+if ( has_post_thumbnail()) {
+
+    $featured_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
+    ?>
+ <div class="mission-wrap" style="background: url('<?php echo $featured_image_url[0]; ?>') no-repeat; background-size: cover;">
+<div class = "palfest-content">
+
   <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+
 </div>
+
+</div>
+
+<?php } 
+else{ ?>
+	<div class = "palfest-content">
+
+  <?php get_template_part('templates/content', 'page'); ?>
+
+</div>
+<?php } ?>
+
+<?php endwhile; ?>
--- web/app/themes/palfest/programme-template.php
+++ web/app/themes/palfest/programme-template.php
@@ -3,18 +3,23 @@
  * Template Name: Programme Template
  */
 ?>
+<?php   
+$args = array( 
+  'post_type' => 'day',
+  'posts_per_page' => 10,
+  'orderby'   => 'date',
+    'order' => 'ASC' 
+  );
+$loop = new WP_Query( $args );           
+if ( has_post_thumbnail()) {
+
+    $featured_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
+    ?>
+      <div class="day-wrap" style="background: url('<?php echo $featured_image_url[0]; ?>') no-repeat; background-size: cover;">
+
 <div class = "container">
 <?php 
 
-$args = array( 
-
-	'post_type' => 'day',
-	'posts_per_page' => 10,
-	'orderby'   => 'date',
-    'order' => 'ASC' 
-
-	);
-$loop = new WP_Query( $args );
 p2p_type( 'profiles_to_days' )->each_connected( $loop );
 echo '<div class = "row">';
 while ( $loop->have_posts() ) : $loop->the_post();
@@ -40,8 +45,48 @@ echo '<div class = "col-sm-6 col-lg-4 programme-day"> <h1>' ;
   
     wp_reset_postdata();
   echo '</div>';
-
 endwhile;
 echo '</div>';
+?>
+</div>
+</div>
+<?php }else{
  ?>
- </div>
+<div class = "container">
+<?php 
+
+p2p_type( 'profiles_to_days' )->each_connected( $loop );
+echo '<div class = "row">';
+while ( $loop->have_posts() ) : $loop->the_post();
+?>
+<a href="<?php the_permalink(); ?>">
+<?
+echo '<div class = "col-sm-6 col-lg-4 programme-day"> <h1>' ;
+  the_title();
+  echo '</h1> <div class="entry-content">';
+  the_content();
+  echo '</a></div>';
+    foreach ( $post->connected as $post ) : setup_postdata( $post );?>
+    
+    <div class = "programme-participant"><p>
+    <a href="<?php the_permalink(); ?>">
+    <?php the_title(); ?>
+    </a>
+    </p>
+    </div>
+   
+    <?php
+    endforeach;
+  
+    wp_reset_postdata();
+  echo '</div>';
+endwhile;
+echo '</div>';
+?>
+</div>
+
+
+<?php } ?>
+
+
+
